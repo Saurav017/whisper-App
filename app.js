4b9bf75736bd99ffca6d6f3884bf338fea61298c
Saurav017 <sauravabde@gmail.com>
2023-03-24 10:11:03 +0530

    New Updates Added

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ recognition.onstart = function() {
     console.log('Voice is activated, you can to microphone');
 };
 
+greetings = ['I am good, how are you ?', 'Doing good homeboi', 'Leave me alone'];
+weather = ['Weather is fine', 'You need a tan'];
+intro = ["My name is Astro, I am your personal assistant. I can help you with your daily tasks. I can tell you the weather, time, date, and many more things. I am still in development phase, so I can't do much right now."];
+
 recognition.onresult = function(event) {
     console.log(event);
     const current = event.resultIndex;
@@ -25,13 +29,20 @@ btn.addEventListener('click', () => {
 
 function readOutLoud(message) {
     const speech = new SpeechSynthesisUtterance();
-    speech.text = message;
-    // speech.text = 'I dont know what you said';
+    speech.text = 'Hey, right Now I dont know what you said. Maybe you can try again later in the upcoming version.';
 
-    // if(message.includes('how are you')) {
-    //     const finalText = greetings[Math.floor(Math.random() * greetings.length)];
-    //     speech.text = finalText;
-    // }
+    if(message.includes('how are you')) {
+        const finalText = greetings[Math.floor(Math.random() * greetings.length)];
+        speech.text = finalText;
+    }
+    else if(message.includes('weather')) {
+        const finalText = weather[Math.floor(Math.random() * weather.length)];
+        speech.text = finalText;
+    }
+    else if(message.includes('introduce yourself')) {
+        const finalText = intro[Math.floor(Math.random() * intro.length)];
+        speech.text = finalText;
+    }
 
     speech.volume = 1;
     speech.rate = 1;
